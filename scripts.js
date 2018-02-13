4a464d90cbfde5af45e61dab4eee6e490cc428d5
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-13 23:02:02 +0100

    Add scripts.js with code

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var a = 5;
+var h = 9;
+var triangleArea = a * h / 2;
+
+prompt('push enter if you want to know the result of the equation a * h / 2');
+alert(+ triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
