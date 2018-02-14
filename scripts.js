7e84f4ae4355c8360a45efe19101bf9fbc0e5da3
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-14 19:58:18 +0100

    Fix code scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 // scripts.js
 
-var a = 5;
-var h = 9;
+var a = prompt('insert a');
+var h = prompt('insert h');
 var triangleArea = a * h / 2;
 
-prompt('push enter if you want to know the result of the equation a * h / 2');
 alert(+ triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
